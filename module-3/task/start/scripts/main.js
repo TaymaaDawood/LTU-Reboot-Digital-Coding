2b9fdf828dbe503e244ec1565fa87fad6a1af81d
TaymaaDawood <82846103+TaymaaDawood@users.noreply.github.com>
2021-05-04 11:22:25 +0100

    Update main.js
    add variables and console log
    add single and multi line comment at the head of js file

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,19 @@
+// This is a single line comment
+
+
+/* 
+This is
+a 
+multiple
+line
+comment
+*/
+
+const productName ='Chocolate Icecream'; //string
+let price =0.99; //float
+let quantity =2; // integer
+let inStock =true; // boolean
+let discountAmount =0.20; //float
+
+//console.log(productName,price);
+
