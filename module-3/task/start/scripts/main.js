ac6385538240ccd118e198ee92748bc3b8a52078
TaymaaDawood <82846103+TaymaaDawood@users.noreply.github.com>
2021-05-05 11:18:14 +0100

    Update main.js
    create a function and change it to arrow fun basic arithmetic

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -10,10 +10,43 @@ comment
 */
 
 const productName ='Chocolate Icecream'; //string
-let price =0.99; //float
+let price =0.89; //float
 let quantity =2; // integer
 let inStock =true; // boolean
 let discountAmount =0.20; //float
 
 //console.log(productName,price);
 
+function showProductName(){
+    console.log(productName);
+}
+//showProductName();
+
+/*function totalPrice(productPrice,productQuantity){
+    console.log(productPrice*productQuantity);
+}
+totalPrice(price,quantity);*/
+
+/*function checkAvailability(productInStock){
+    console.log(productInStock);
+}
+checkAvailability(inStock);*/
+
+/*let checkAvailability=productInStock => console.log(productInStock);
+checkAvailability(inStock);*/
+
+/*function showMessege(){
+    let message = 'Hello, I am JavaScript';
+    alert (message);
+}
+showMessege();*/
+function productDiscount(){
+    if (quantity > 1){
+     let newPrice=(price - discountAmount * quantity);
+    console.log(newPrice);
+    }
+    else{
+        console.log(price*quantity);
+    }
+}
+productDiscount();
