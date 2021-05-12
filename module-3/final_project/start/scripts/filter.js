6212b3628690118b1f67a565ee02108c0d962a39
TaymaaDawood <82846103+TaymaaDawood@users.noreply.github.com>
2021-05-12 11:28:12 +0100

    Create filter.js
    select buttons and images using DOM

---
+++ module-3/final_project/start/scripts/filter.js
@@ -0,0 +1,42 @@
+const buttons= document.querySelectorAll('button');
+
+let allImages= document.querySelectorAll('.image');
+
+let images = document.querySelectorAll('img');
+
+function toggleImages(dataClass){
+
+}
+
+let person={
+    name:'Taymaa',
+    age:36,
+    height: 165,
+    birthplace:'Nineveh'
+}
+
+person['age'];
+person.age;
+person.birthplace;
+
+
+//buttons change colour to blue and text to white
+
+/*for (let counter =0; counter <buttons.length; counter++){
+    buttons[counter].style.backgroundColor='blue';
+    buttons[counter].style.color='white';
+}
+
+buttons.forEach(button =>  {
+    button.style.backgroundcolor='blue';
+    button.style.color='white';
+});
+
+
+for (let i =0; i<images.length; i++){
+
+ images[i].setAttribute('src','https://via.placeholder.com/150');
+}*/
+
+
+
